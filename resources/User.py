abccff82624b81343f666bf96a7ecaecfd1480ba
Phadnis <bhagvant.phadnis@kpn.com>
2021-06-10 18:41:44 +0530

    Logging in with Flask-JWT-Extended

--- resources/User.py
+++ resources/User.py
@@ -1,24 +1,26 @@
-import sqlite3
+from werkzeug.security import safe_str_cmp    # for safe string compare in all python versions
 from flask_restful import Resource,reqparse     #reqparse for request parsing
+from flask_jwt_extended import create_access_token, create_refresh_token
 from models.User import UserModel
 
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username',
+        type=str,
+        required=True,
+        help="This field can not be blank!"
+    )
+_user_parser.add_argument('password',
+            type=str,
+        required=True,
+        help="This field can not be blank!"
+    )
 
 class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-        type=str,
-        required=True,
-        help="This field can not be blank!"
-    )
-    parser.add_argument('password',
-        type=str,
-        required=True,
-        help="This field can not be blank!"
-    )
+
 
     def post(self):
 
-        data = UserRegister.parser.parse_args()
+        data = _user_parser.parse_args()
 
         # Prevent duplicate user signing
 
@@ -47,3 +49,25 @@ class User(Resource):
             return {'message':'User not found'},404
         user.delete_from_db()
         return {'message':'User deleted'},200
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        #get data from parser
+        data = _user_parser.parse_args()
+
+        #find user in DATABASE
+        user = UserModel.find_by_username(data['username'])
+        #check password
+        if user and safe_str_cmp(user.password, data['password']):
+            #create access tokan
+            access_token = create_access_token(identity=user.id, fresh=True)
+            #create refresh token
+            refresh_token = create_refresh_token(user.id)
+            return {
+                'access_token':access_token,
+                'refresh_token':refresh_token
+            }, 200
+        #return them
+        return {'message':'Invalid credentials'}, 401
